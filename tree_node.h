20e8f2c2244eb7feb78d1eb4206e4e7f4172c71e
Kennedy Firth <kf766622@ohio.edu>
2026-04-20 15:35:25 -0400

    comments in tree_node.h

--- tree_node.h
+++ tree_node.h
@@ -1,3 +1,10 @@
+/*
+ * @file tree_node.h
+ * @author Kennedy Firth and Megan Penn
+ * @brief - Class for the nodes to build a tree 
+ * @date 2026-04-20
+*/
+
 #ifndef TREE_NODE_H
 #define TREE_NODE_H
 
@@ -5,20 +12,28 @@
 #include <vector>
 #include <iostream>
 using namespace std;
-
+//class for Nodes
 class Node{
     public:
         string name;
         int weight;
         string parent;
         vector<Node*> children;
+    /** 
+     * @brief constructor
+     * @param n the name of the node
+     * @param w the weight
+     * @param p the parent of this node
+     */
 
     Node(string n, int w, string p = "") {
         name = n;
         weight = w;
         parent = p;
     }
-
+    /**
+     * @brief the print function for the tree
+     */
     void print() {
         cout << name;
         if (!children.empty()) {
